379e2f6c3b25817421be10e6213d871540962bde
Developer64 <simlim2013@yandex.ru>
2023-08-30 23:55:05 +0300

    second commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let obj = [
+{
+    "color":"Tomato",
+    "numbers":["88888", "77777"],
+    "checked": true 
+},
+{
+    "color":"Tomato",
+    "numbers":["88888", "77777"],
+    "checked": true 
+},
+{
+    "color":"Tomato",
+    "numbers":["88888", "77777"],
+    "checked": true 
+},
+]
